9d85029dedaea880bb87aa00a0c19fa98deacdf8
Zax-Yzotope <isa.marchand@outlook.com>
2020-04-03 19:05:56 -0400

    delete some useless files

--- start.js
+++ start.js
@@ -6,37 +6,6 @@ const schema = require('./schema');
 
 const mysql = require('mysql');
 
-// var con = mysql.createConnection({
-//     host: "localhost",
-//     port: "3306",
-//     user: "jeunex",
-//     password: "password",
-//     database: "Application_Development"
-// });
-
-// con.connect(function(err) {
-//     if (err) throw err;
-//     console.log("mysql connected... je pense");
-//     console.log(con.config.database)
-    
-// });
-
-// connexion to PG
-// var conpg = pg.createConnection({
-//     host: "localhost",
-//     port: "5432",
-//     user: "postgres",
-//     password: "patate",
-//     database: "datawarehouse"
-
-// });
-
-// conpg.connect(function(err) {
-//     if (err) throw err;
-//     console.log("pg connected, PATATE!");
-//     // console.log(con.config.database)
-    
-// });
 
 app.use('/graphql', graphqlHTTP({
     schema,
@@ -44,11 +13,8 @@ app.use('/graphql', graphqlHTTP({
 
 }));
 
-// app.listen(4000,() => {
-//     console.log("It is listenning, patate!")
-// })
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(" PATATE Our app is running on port");
+    console.log("Our app is running on port");
 });
